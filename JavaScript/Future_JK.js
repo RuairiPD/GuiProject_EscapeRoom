3483e2897fb6f96f021b2a87f8f69c7951b1505b
JamieKell05 <G00464892@atu.ie>
2025-04-29 21:24:54 +0100

    various
    finished future page and just have very little to tweak. Changed storage to session so you can retest key functionality. Feel free to change back to local if needed

--- JavaScript/Future_JK.js
+++ JavaScript/Future_JK.js
@@ -4,15 +4,33 @@ let userCheck;
 let finalCheck = userCheck.value.toLowerCase();
 
     if (finalCheck==="trusted data exchange") {
-        sessionStorage.futureKey = "Y";
+        sessionStorage.setItem("key_future",1);
+        let img = document.createElement("img");
 
+        // Set the source, width, 
+        // height, and alt attributes
+        img.src = "images/key.png";
+        img.width = 250;
+        img.height = 200;
+        img.alt = "key";
+
+        // Add positioning style
+        img.style.position = "absolute"; // or "relative", "fixed", etc.
+        img.style.left = "610px";        // X-coordinate
+        img.style.top = "200px";          // Y-coordinate
+
+    
+        // Append the image element
+        // to the body of the document
+        document.body.appendChild(img);
     }
 
     else{
-        sessionStorage.futureKey = "N";
+        sessionStorage.setItem("key_future",0);
 
     }
 
 
 
 }
+
