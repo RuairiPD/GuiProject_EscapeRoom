f53b2d7db31931e1eb9d747a76d9ba247b36b65b
Gartha-dv <G00468877@atu.ie>
2025-05-01 13:41:09 +0100

    MAJORITY done

--- PastJS/Past.js
+++ PastJS/Past.js
@@ -1,15 +1,19 @@
 alert("This man has been condemned to die!");
 alert("Find the correct word to set him free and acquire the key!");
 
+document.addEventListener("DOMContentLoaded", () => {
+
 const words = ["javascript", "hangman", "developer", "browser", "function"];
 const word = words[Math.floor(Math.random() * words.length)];
 let guessed = [];
 let wrong = [];
+let Count = 0;
 
 const wordDiv = document.getElementById("word");
 const wrongDiv = document.getElementById("wrong-guesses");
 const messageDiv = document.getElementById("message");
 const keyboardDiv = document.getElementById("keyboard");
+const hangmanImage = document.getElementById("hangman-image");
 
 function updateWordDisplay() {
   wordDiv.innerHTML = word
@@ -20,9 +24,6 @@ function updateWordDisplay() {
 
 function checkGameStatus() {
   if (wrong.length >= 6) {
-    messageDiv.textContent = `You lost! The word was "${word}".`;
-    disableAllButtons();
-  } else if (word.split("").every(letter => guessed.includes(letter))) {
     messageDiv.textContent = "Congratulations! You won!";
     disableAllButtons();
   }
@@ -50,11 +51,45 @@ function handleGuess(letter, btn) {
   } else {
     wrong.push(letter);
     wrongDiv.textContent = wrong.join(" ");
+
+    
+    Count++;
+      if (Count === 0) {
+        hangmanImage.src = 
+        `https://media.geeksforgeeks.org/wp-content/uploads/20240215173028/0.png`;
+      }
+      if (Count === 1) {
+        hangmanImage.src = 
+        `https://media.geeksforgeeks.org/wp-content/uploads/20240215173033/1.png`;
+      }
+      if (Count === 2) {
+        hangmanImage.src = 
+        
+        `https://media.geeksforgeeks.org/wp-content/uploads/20240215173038/2.png`;
+      }
+      if (Count === 3) {
+        hangmanImage.src = 
+        `https://media.geeksforgeeks.org/wp-content/uploads/20240215172733/3.png`;
+      }
+      if (Count == 4) {
+        hangmanImage.src = 
+        `https://media.geeksforgeeks.org/wp-content/uploads/20240215173815/4.png`;
+      }
+      if (Count === 5) {
+        hangmanImage.src = 
+        `https://media.geeksforgeeks.org/wp-content/uploads/20240215173859/5.png`;
+      }
+      if (Count === 6) {
+        hangmanImage.src =
+        `https://media.geeksforgeeks.org/wp-content/uploads/20240215173931/6.png`;
+      }
+      // hangmanImage.src = 
+      `images/hangman-${Count}.svg`;
+    }
   }
+
   updateWordDisplay();
   checkGameStatus();
-}
+  createKeyboard();
 
-// Initialize
-updateWordDisplay();
-createKeyboard();
+})
